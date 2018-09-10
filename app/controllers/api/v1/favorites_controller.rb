9fef05cbc984f3ea1c9a185b1e99013d46750fc6
laurkim <laurakeem@gmail.com>
2018-09-10 17:25:08 -0400

    Add functionality to favorite a workspace

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :requires_login, only: [:create]
+
   def create
+    user_id = decode_token
+    @favorite = Favorite.new(favorites_params)
+    @favorite.user_id = user_id
+    @favorite.save
+    render json: @favorite
   end
 
   private
 
-  def favorites_params 
+  def favorites_params
+    params.require(:favorite).permit(:workspace_id)
   end
 end
