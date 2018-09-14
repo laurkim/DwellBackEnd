c1a810c1de4f8c6d1142711cb7eae494b17774f9
laurkim <laurakeem@gmail.com>
2018-09-14 16:53:31 -0400

    Remove authorization check before rendering all workspaces for deployment purposes

--- app/controllers/api/v1/workspaces_controller.rb
+++ app/controllers/api/v1/workspaces_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::WorkspacesController < ApplicationController
-  before_action :requires_login, only: [:index]
-
   def index
     @workspaces = Workspace.all
     render json: @workspaces
   end
-
 end
